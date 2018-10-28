0dded0e20ef88dc9f78af885e279a7b29e14cbfb
suyash kumar singh <suyash.singh@outlook.com>
2018-10-29 00:24:28 +0530

    order menu ui

--- src/ui/src/app/menu.service.ts
+++ src/ui/src/app/menu.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Menu} from './ordermenu/Menu'
-
+import { Category } from './ordermenu/category';
+import {Item} from './ordermenu/Item'
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
- 
+  orderMenu:Menu["menus"][]=[];
+  menuList:Category[]=[];
+  categoryList:Item[]=[];
+  itemList:Item[]=[];
+  itemGroupList:Item[]=[];
+  
  
   constructor(private httpClient:HttpClient) { }
 
@@ -15,4 +21,71 @@ export class MenuService {
    return this.httpClient.get<Menu[]>('src/app/menu.json');
   
   }
+
+  getMenuList():void{
+    this.getMenuJSON().subscribe(o=>{
+        
+      this.orderMenu["menus"]=o;
+      
+      o["menus"].forEach(element => {
+        var menu=new Category();
+        menu.id=element.menu.id;
+        menu.name=element.menu.name;
+        menu.items=[];
+        this.menuList.push(menu);
+        element.menu.categories.forEach(eleCat => {
+          var cat=new Item();
+          cat.pid=menu.id;
+          cat.id=eleCat.category.id;
+          cat.name=eleCat.category.name;
+          
+          this.categoryList.push(cat);
+          menu.items.push(cat);
+          cat.item=[];
+          eleCat.category.items.forEach(eleItem => {
+            var item=new Item();
+            item.pid=cat.id;
+            item.id=eleItem.item.id;
+            item.name=eleItem.item.name;
+            item.price=eleItem.item.price;
+            item.quantity=0;
+            this.itemList.push(item);
+            cat.item.push(item);
+            item.item=[];
+            if(eleItem.item.groups){
+              eleItem.item.groups.forEach(eleGroup => {
+                
+                eleGroup.group.items.forEach(eleGroupItem => {
+                  var itemGroup=new Item();
+                  itemGroup.pid=item.id;
+                  itemGroup.id=eleGroupItem.item.id;
+                  itemGroup.name= eleGroup.group.name+'-'+eleGroupItem.item.name;
+                  itemGroup.price=eleGroupItem.item.price;
+                  this.itemGroupList.push(itemGroup);
+                  item.item.push(itemGroup);
+                  
+                });
+              });
+            }
+          
+          });
+
+
+        });
+        
+      });
+      
+    console.log(o["menus"]); 
+    console.log(this.menuList); 
+    console.log(this.categoryList); 
+    console.log(this.itemList); 
+    console.log(this.itemGroupList); 
+
+        
+
+    });
+ 
+}
+
+
 }
--- src/ui/src/app/ordermenu/ordermenu.component.ts
+++ src/ui/src/app/ordermenu/ordermenu.component.ts
@@ -1,90 +1,24 @@
 import { Component, OnInit, Inject,Renderer2 } from '@angular/core';
 import {MenuService} from '../menu.service'
-import {Menu} from './Menu'
-import { Category } from './category';
-import {Item} from './Item'
+ 
+
  import { DOCUMENT } from '@angular/common';
 import {ViewChild, ElementRef} from '@angular/core';
+import { Category } from './Category';
+import { Item } from './Item';
 @Component({
   selector: 'app-ordermenu',
   templateUrl: './ordermenu.component.html',
   styleUrls: ['./ordermenu.component.scss']
 })
 export class OrdermenuComponent implements OnInit {
-orderMenu:Menu["menus"][]=[];
-menuList:Category[]=[];
-categoryList:Item[]=[];
-itemList:Item[]=[];
-itemGroupList:Item[]=[];
-currentCategory:string;
-
+  cartItem:Item[]=[];
+  totalPrice:number=0;
+  currentCategory:string;
    
   constructor (private menuService:MenuService,  @Inject(DOCUMENT) private document: Document,
     private renderer: Renderer2) { }
-getMenuList():void{
-    this.menuService.getMenuJSON().subscribe(o=>{
-        
-      this.orderMenu["menus"]=o;
-      
-      o["menus"].forEach(element => {
-        var menu=new Category();
-        menu.id=element.menu.id;
-        menu.name=element.menu.name;
-        menu.items=[];
-        this.menuList.push(menu);
-        element.menu.categories.forEach(eleCat => {
-          var cat=new Item();
-          cat.pid=menu.id;
-          cat.id=eleCat.category.id;
-          cat.name=eleCat.category.name;
-          
-          this.categoryList.push(cat);
-          menu.items.push(cat);
-          cat.item=[];
-          eleCat.category.items.forEach(eleItem => {
-            var item=new Item();
-            item.pid=cat.id;
-            item.id=eleItem.item.id;
-            item.name=eleItem.item.name;
-            item.price=eleItem.item.price;
-            item.quantity=0;
-            this.itemList.push(item);
-            cat.item.push(item);
-            item.item=[];
-            if(eleItem.item.groups){
-              eleItem.item.groups.forEach(eleGroup => {
-                
-                eleGroup.group.items.forEach(eleGroupItem => {
-                  var itemGroup=new Item();
-                  itemGroup.pid=item.id;
-                  itemGroup.id=eleGroupItem.item.id;
-                  itemGroup.name= eleGroup.group.name+'-'+eleGroupItem.item.name;
-                  itemGroup.price=eleGroupItem.item.price;
-                  this.itemGroupList.push(itemGroup);
-                  item.item.push(itemGroup);
-                  
-                });
-              });
-            }
-          
-          });
-
-
-        });
-        
-      });
-      
-    console.log(o["menus"]); 
-    console.log(this.menuList); 
-    console.log(this.categoryList); 
-    console.log(this.itemList); 
-    console.log(this.itemGroupList); 
-
-        
-
-    });
- 
-}
+   
 catSelect(id:string,event:any):void{
   this.currentCategory=id;
   event.preventDefault();
@@ -93,13 +27,30 @@ catSelect(id:string,event:any):void{
   this.currentCategory=id;
 
 }
+addMenu(item:Item,quantity:number):void{
+  
+  item.quantity=item.quantity+quantity;
+  var singleItem:Item=new Item();
+  singleItem=this.cartItem.find(o=>o.id==item.id);
+  if(!singleItem){
+    this.cartItem.push(item);
+  }
+  if(item.quantity<=0){
+    item.quantity=0;
+    var index=this.cartItem.indexOf(singleItem);
+    this.cartItem.splice(index,1);
+  }
+ 
+  this.totalPrice=this.totalPrice+quantity*item.price;
+}
 @ViewChild('firstNameInput') nameInputRef: ElementRef;
 show(lastName: HTMLInputElement){
   
   this.nameInputRef.nativeElement.value + ' ' + lastName.value;
 }
+
   ngOnInit() {
-    this.getMenuList();
+    this.menuService.getMenuList();
    
   }
 
